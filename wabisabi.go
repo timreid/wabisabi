2e91dc7dd8c9fd3271bdb2ad5676b43728712ba3
tim reid <timreid@gmail.com>
2014-04-18 01:30:16 +0000

    framework basically completed

--- wabisabi.go
+++ wabisabi.go
@@ -1,4 +1,4 @@
-package floop2
+package wabisabi
 
 type Individual interface{}
 
@@ -15,7 +15,8 @@ type ScoringFunction func(Individual) float64
 
 type IndividualSelection func(Population) ScoredIndividual
 
-type Operator func([]ScoredIndividual) []ScoredIndividual
+type Operator func(IndividualSelection, Population, ScoringFunction) []ScoredIndividual
+
 type Operators []Operator
 
 type OperatorSelection func(Operators) Operator
@@ -25,13 +26,38 @@ type Monitor func(State, Population) (State, bool)
 type Propagator func(State, Population) Population
 
 func MakePropagator(scoring ScoringFunction, individualSelection IndividualSelection, operatorSelection OperatorSelection, operators Operators) Propagator {
-	return
+	propagator := func(state State, population Population) Population {
+		populationSize := state["populationSize"].(int)
+		eliteGroupSize := state["eliteGroupSize"].(int)
+		k := populationSize - eliteGroupSize
+		newPopulation := make(Population, populationSize)
+		//create children until the newPopulation is full
+		for j := 0; j < k; {
+			//select operator
+			chosenOperator := operatorSelection(operators)
+			//create variable number of children
+			children := chosenOperator(individualSelection, population, scoring)
+			//copy chlidren into pop (copy is smart about not overfilling)
+			taken := copy(newPopulation, children)
+			j += taken
+		}
+		copy(newPopulation, population[0:eliteGroupSize])
+		return newPopulation
+	}
+	return propagator
 }
 
 func Evolve(scoring ScoringFunction, individualSelection IndividualSelection, operatorSelection OperatorSelection, monitor Monitor, initialPopulation Population, initialState State, operators Operators) (State, Population) {
-	generation := func(state State, population Population) (State, Population) {
+	generation := func(state State, population Population) Population {
 
-		return
+		return population
 	}
-	return
+	state := initialState
+	population := initialPopulation
+	done := false
+	for !done {
+		population = generation(state, population)
+		state, done = monitor(state, population)
+	}
+	return state, population
 }
