c477a923e0237d2287c9b6e0c4f8a465e1d7b83d
tim reid <timreid@gmail.com>
2014-05-01 18:52:56 +0000

    bit o refactoring

--- wabisabi.go
+++ wabisabi.go
@@ -1,63 +1,126 @@
 package wabisabi
 
-type Individual interface{}
-
-type ScoredIndividual struct {
-	Score      float64
-	Individual Individual
-}
-
-type Population []ScoredIndividual
+import "math"
+import "math/rand"
+import "fmt"
+import "sort"
 
 type State map[string]interface{}
 
-type ScoringFunction func(Individual) float64
+type Genotype interface{}
 
-type IndividualSelection func(Population) ScoredIndividual
+type Individual struct {
+	Score  float64
+	State  State
+	Genome Genotype
+}
 
-type Operator func(IndividualSelection, Population, ScoringFunction) []ScoredIndividual
+type Population []Individual
 
-type Operators []Operator
+func (p Population) Len() int {
+	return len(p)
+}
 
-type OperatorSelection func(Operators) Operator
+func (p Population) Less(i, j int) (x bool) {
+	tempIndI := p[i]
+	tempIndJ := p[j]
+	if tempIndI.Score < tempIndJ.Score || math.IsNaN(tempIndI.Score) {
+		x = false
+	} else {
+		x = true
+	}
+	return x
+}
+
+func (p Population) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+	return
+}
+
+type Fitness func(Genotype) (float64, State)
+
+type Selection func(Population) Individual
+
+type Operator struct {
+	P  func(State) float64
+	Op func(Fitness, Selection, Population) Population
+}
 
 type Monitor func(State, Population) (State, bool)
 
-type Propagator func(State, Population) Population
+func SimpleMonitor(state State, population Population) (newState State, done bool) {
+	generation := state["generation"].(int)
+	max := state["maxFitness"].(float64)
+	goal := state["goalFitness"].(float64)
+	sum := 0.0
+	for _, c := range population {
+		//min, max, mean, std dev
+		cs := c.Score
 
-func MakePropagator(scoring ScoringFunction, individualSelection IndividualSelection, operatorSelection OperatorSelection, operators Operators) Propagator {
-	propagator := func(state State, population Population) Population {
+		if cs > max {
+			max = cs
+		}
+		if !math.IsNaN(cs) {
+			sum = sum + cs
+		}
+	}
+	newState = state
+	mean := sum / float64(len(population))
+	newState["generation"] = generation + 1
+	newState["maxFitness"] = max
+	newState["meanFitness"] = mean
+	fmt.Printf("%4d\t%4.3f\t%4.3f", generation, max, mean)
+	fmt.Println()
+	if max >= goal {
+		done = true
+	}
+	return
+}
+
+func Evolve(fitness Fitness, selection Selection, monitor Monitor, operators []Operator, initialPopulation Population, initialState State) (State, Population) {
+
+	state := initialState
+	state["generation"] = 0
+	population := initialPopulation
+	done := false
+
+	operatorSelection := func(operators []Operator) Operator {
+		i := 0
+		x := rand.Float64()
+		for P := operators[i].P(state); x > P; P = operators[i].P(state) {
+			x = x - P
+			i++
+		}
+		return operators[i]
+
+	}
+
+	propagate := func(state State, population Population) Population {
 		populationSize := state["populationSize"].(int)
 		eliteGroupSize := state["eliteGroupSize"].(int)
 		k := populationSize - eliteGroupSize
-		newPopulation := make(Population, populationSize)
+		newPopulation := *new(Population)
 		//create children until the newPopulation is full
 		for j := 0; j < k; {
 			//select operator
 			chosenOperator := operatorSelection(operators)
-			//create variable number of children
-			children := chosenOperator(individualSelection, population, scoring)
-			//copy chlidren into pop (copy is smart about not overfilling)
-			taken := copy(newPopulation, children)
-			j += taken
+			//create some variable number of children
+			children := chosenOperator.Op(fitness, selection, population)
+			nChildren := len(children)
+			//fmt.Println("children: ", children)
+			newPopulation = append(newPopulation, children...)
+			j += nChildren
 		}
-		copy(newPopulation, population[0:eliteGroupSize])
+		//copy elite into pop
+		newPopulation = append(newPopulation[0:k], population[0:eliteGroupSize]...)
+		sort.Sort(newPopulation)
 		return newPopulation
 	}
-	return propagator
-}
 
-func Evolve(scoring ScoringFunction, individualSelection IndividualSelection, operatorSelection OperatorSelection, monitor Monitor, initialPopulation Population, initialState State, operators Operators) (State, Population) {
-	generation := func(state State, population Population) Population {
-
-		return population
-	}
-	state := initialState
-	population := initialPopulation
-	done := false
 	for !done {
-		population = generation(state, population)
+		population = propagate(state, population)
 		state, done = monitor(state, population)
 	}
+
 	return state, population
 }
